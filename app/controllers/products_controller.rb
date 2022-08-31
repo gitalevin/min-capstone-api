f12e15a45bd6247e3ea2482ceba0c8556d87c279
gitalevin <reachglevin@gmail.com>
2022-08-31 18:56:31 -0500

    index and show controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,37 @@
 class ProductsController < ApplicationController
-  get "/products", controller: "products", action: "products_method"
+  def index
+    product = Product.all
+    render json: product.as_json
+  end
+  def create
+    product = Product.new(
+      name: "Orange"
+      price: 4
+      images_url: "wwww.orange4.com"
+      description: "citrus"
+    )
+    product.save
+    render json: product.as_json
+  def show
+    product = Product.find_by(id: params["id"])
+    render json: product.as_json
+  end
+
+  def apple_products_method
+    product = Product.first
+    render json: product.as_json
+  end
+
+  def mango_products_method
+    product = Product.second
+    render json: product.as_json
+  end
+
+  def banana_products_method
+    product = Product.third
+    render json: product.as_json
+  end
+  def one_product_method
+    product = Product.find_by(id: params["id"])
+    render json: product.as_json
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/all_products", controller: "products", action: "all_products_method"
+  get "/apple_products", controller: "products", action: "apple_products_method"
+  get "/mango_products", controller: "products", action: "mango_products_method"
+  get "/banana_products", controller: "products", action: "banana_products_method"
+  get "/one_product/:id", controller: "products", action: "one_product_method"
+  get "/products" => "rproducts#index"
+  get "/products/:id" => "products#show"
 end
