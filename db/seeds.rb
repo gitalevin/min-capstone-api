cb733fe800e68dd3af1a2ce7df0220497010abc5
gitalevin <reachglevin@gmail.com>
2022-08-30 19:19:54 -0500

    Created products using db/seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+product = Product.new(name: "apple", price: 2, image_url: "www.apple2.com", description: "green")
+product.save
+product = Product.new(name: "mango", price: "3", image_url: "www.mango3.com", description: "juicy")
+product.save
+product = Product.new(name: "banana", price: "5", image_url: "www.banana4.com", description: "ripe")
+product.save
